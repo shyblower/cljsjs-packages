35eea5ba7528bf9ba8b95ff2efcefbbfb90dc0be
Thomas Scheiblauer <tom@sharkbay.at>
2016-05-17 17:36:58 +0200

    fix webpack.config.js

--- react-overlays/webpack.config.js
+++ react-overlays/webpack.config.js
@@ -1,14 +1,10 @@
 module.exports = {
     output: {
-        // export itself to a global var
         libraryTarget: "var",
-        // name of the global var: "Foo"
         library: "ReactOverlays"
     },
     externals: {
-        // require("jquery") is external and available
-        //  on the global var jQuery
         "react": "React",
-        "react-dom": "ReactDom"
+        "react-dom": "ReactDOM"
     }
 }
